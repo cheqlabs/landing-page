75ff09a7c63cd9a0f37bd4d7296bac8496696cdb
Rafi <rafi337161@gmail.com>
2022-08-18 19:26:53 -0400

    made toggle button 'ghost'

--- pages/components/ToggleColorMode.tsx
+++ pages/components/ToggleColorMode.tsx
@@ -1,17 +1,18 @@
-import { Button, useColorMode } from "@chakra-ui/react";
-import { MoonIcon, SunIcon } from "@chakra-ui/icons";
+import { Button, useColorMode } from '@chakra-ui/react';
+import { MoonIcon, SunIcon } from '@chakra-ui/icons';
 
 function ToggleColorMode() {
   const { colorMode, toggleColorMode } = useColorMode();
   return (
     <Button
       onClick={() => toggleColorMode()}
-      pos="absolute"
-      top="0"
-      right="0"
-      m="1rem"
+      pos='absolute'
+      top='0'
+      right='0'
+      m='1rem'
+      variant='ghost'
     >
-      {colorMode === "dark" ? <SunIcon /> : <MoonIcon />}
+      {colorMode === 'dark' ? <SunIcon /> : <MoonIcon />}
     </Button>
   );
 }
