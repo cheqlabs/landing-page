226d0b15145cf3de1476de58e66230239e64b341
Rafi <rafi337161@gmail.com>
2022-08-18 14:53:26 -0400

    added logo

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,10 +5,15 @@ import {
   Stack,
   Text,
   Button,
+  useColorMode,
 } from "@chakra-ui/react";
+import Image from "next/image";
+import lightLogo from "../public/cheq light logo chakra.png";
+import darkLogo from "../public/cheq dark logo chakra.png";
 import ToggleColorMode from "./components/ToggleColorMode";
 
 function HomePage() {
+  const { colorMode, toggleColorMode } = useColorMode();
   return (
     <Container maxW={"5xl"}>
       <ToggleColorMode />
@@ -18,24 +23,29 @@ function HomePage() {
         spacing={{ base: 8, md: 10 }}
         py={{ base: 20, md: 28 }}
       >
+        {colorMode === "light" ? (
+          <Image
+            src={lightLogo}
+            alt="Cheq light logo"
+            width={256}
+            height={314}
+          />
+        ) : (
+          <Image src={darkLogo} alt="Cheq dark logo" width={256} height={314} />
+        )}
         <Heading
           fontWeight={600}
           fontSize={{ base: "3xl", sm: "4xl", md: "6xl" }}
           lineHeight={"110%"}
         >
-          <Text as={"span"} color={"orange.400"}>
-            Cheq
-          </Text>{" "}
-          Protocol
+          Cheq Protocol
         </Heading>
         <Text color={"gray.500"} maxW={"3xl"}>
           Cheq is a decentralized payments protocol for reversible, tradable
           transactions that powers deNota.
         </Text>
         <Stack spacing={6} direction={"row"}>
-          <Button px={6}>
-            Contact us
-          </Button>
+          <Button px={6}>Contact us</Button>
         </Stack>
         <Flex w={"full"}></Flex>
       </Stack>
--- theme.tsx
+++ theme.tsx
@@ -1,24 +1,10 @@
 import { extendTheme, type ThemeConfig } from '@chakra-ui/react';
 
-const colors = {
-  brand: {
-    50: '#fff6da',
-    100: '#ffe4ad',
-    200: '#ffd27d',
-    300: '#ffc04b',
-    400: '#ffae1a',
-    500: '#e69500',
-    600: '#b37300',
-    700: '#815300',
-    800: '#4e3200',
-    900: '#1e1000',
-  },
-};
 const config: ThemeConfig = {
   initialColorMode: 'dark',
   useSystemColorMode: false,
 } 
 
-const customTheme = extendTheme({ colors, config });
+const customTheme = extendTheme({ config });
 
 export default customTheme;
